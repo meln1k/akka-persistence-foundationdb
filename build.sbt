94cdfc0aca9daeba05db5432e7a8ee8a47c323ea
Odd Möller <odd.moller@gmail.com>
2018-10-15 08:51:34 +0200

    Upgrade Scala, SBT, Akka and all other dependencies to the latest versions.

--- build.sbt
+++ build.sbt
@@ -6,7 +6,7 @@ name := "akka-persistence-foundationdb"
 
 version := "0.1.0"
 
-scalaVersion := "2.12.6"
+scalaVersion := "2.12.7"
 
 scalacOptions ++= Seq(
   "-encoding",
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,20 +2,20 @@ import sbt._
 
 object Dependencies {
 
-  val AkkaVersion = "2.5.11"
+  val AkkaVersion = "2.5.17"
 
   val akkaPersistenceFoundationDependencies = Seq(
     "org.scala-lang.modules" %% "scala-async"                         % "0.9.7",
     "com.typesafe.akka"      %% "akka-persistence"                    % AkkaVersion,
     "com.typesafe.akka"      %% "akka-cluster-tools"                  % AkkaVersion,
     "com.typesafe.akka"      %% "akka-persistence-query"              % AkkaVersion,
-    "com.google.protobuf"     % "protobuf-java"                       % "3.6.0",
+    "com.google.protobuf"     % "protobuf-java"                       % "3.6.1",
     "org.foundationdb"        % "fdb-java"                            % "5.2.5",
 
 
     "com.typesafe.akka"      %% "akka-persistence-tck"                % AkkaVersion     % "test",
     "com.typesafe.akka"      %% "akka-stream-testkit"                 % AkkaVersion     % "test",
     "ch.qos.logback"          % "logback-classic"                     % "1.2.3"         % "test",
-    "org.scalatest"          %% "scalatest"                           % "3.0.4"         % "test"
+    "org.scalatest"          %% "scalatest"                           % "3.0.5"         % "test"
   )
 }
